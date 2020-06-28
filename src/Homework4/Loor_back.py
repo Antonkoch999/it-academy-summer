e1ec38ed1aa0a1aff98a7756e93eff229509b860
Anton Kochnevski <Antonkoch999@gmail.com>
2020-06-28 11:52:47 +0300

    'remake_loor_back'

--- src/Homework4/Loor_back.py
+++ src/Homework4/Loor_back.py
@@ -7,32 +7,21 @@
 
 
 def evklid(a, b):
-    number_one = a
-    number_two = b
-    lst = []
+    if a < b:
+        a, b = b, a
     while a > 0:
         if b <= 0:
             break
         a = a % b
         if a <= 0:
             break
-        elif b % a == 0:
-            b = b // a
-        else:
-            b = b % a
-        lst.append(a)
-        lst.append(b)
-    lst1 = []
-    for el in lst:
-        if el == 1:
-            continue
-        elif el in lst1:
-            continue
-        elif number_one % el == 0 and number_two % el == 0:
-            lst1.append(el)
-    return max(lst1)
+        b = b % a
+    if a > b:
+        return a
+    else:
+        return b
 
 
 if __name__ == '__main__':
-    a, b = 432, 324
+    a, b = 594, 7920
     print(evklid(a, b))
