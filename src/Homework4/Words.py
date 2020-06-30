75005c8f85a4fc7a4fb3b3dda757d55901ec3150
Anton Kochnevski <Antonkoch999@gmail.com>
2020-06-30 21:26:00 +0300

    'pydodestyle'

--- src/Homework4/Words.py
+++ src/Homework4/Words.py
@@ -12,9 +12,9 @@ def words(str_):
     str_.splitlines()
     str1 = ''.join(str_)
     lst = str1.split()
-    return len(set(lst))
+    return len(set(lst)), set(lst)
 
 
 if __name__ == '__main__':
-    str_ = 'asdwdw, asdwdw, asdwdw, asdwdw. asdwdw:  asdwqdqw  b\n    m  asdawdaw a ddasdac  d\n'
+    str_ = 'asdwdw, asdwdw, asdwdw. asdwdw: b\n    m  a ddasdac  d\n'
     print(words(str_))
